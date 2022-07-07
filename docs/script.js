af7c7ef34ba656d2f829c7d560bc6358264d521e
Dokeyz <rikens1996@gmail.com>
2022-07-07 11:04:01 -0400

    Start quiz now goes to next page

--- docs/script.js
+++ docs/script.js
@@ -1,19 +1,37 @@
-var timer = function() {
-let counter = 60;
+/*const startButton = document.getElementById('main');*/
 
-var countInterval = setInterval(count, 1000);
+quiz=function(){
 
-function count() {
-    console.log(counter--);
-    document.getElementById("js_timer").innerHTML = "Timer: " + counter;
-    if (counter === -1) {
-        clearInterval(countInterval);
+var timer = function(){
+    let counter = 60;
+
+    var countInterval = setInterval(count, 1000);
+
+    function count() {
+        console.log(counter--);
+        document.getElementById("js_timer").innerHTML = "Timer: " + counter;
+        if (counter === -1) {
+            clearInterval(countInterval);
+        }
     }
+    const main = document.getElementById('main')
+    main.classList.add('hide')
+    const question1 = document.getElementById('question1')
+    question1.classList.remove('hide') 
+    removeEventListener('click',timer)
+    addEventListener('click',Question1)
+    
 }
-removeEventListener('click',timer)
+addEventListener('click',timer)
+
+/*function mainpage() {
+    const main = document.getElementById('main')
+    main.classList.add('hide')
+    const question1 = document.getElementById('question1')
+    question1.classList.remove('hide') 
+    removeEventListener('click',mainpage)
+}
+*/
 }
 
-
-
-addEventListener('click', timer) 
-
+quiz();
